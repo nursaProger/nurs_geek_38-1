9c695a776f5a3dc164c58fdeaedb09c78ae20976
nursaProger <nursakeneshbb06@gmail.com>
2024-02-19 22:47:09 +0600

    hw-1

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,8 @@
 from aiogram import types, Dispatcher
 from config import bot
+from database import bot_db
+from keyboards import start_inline_buttons
+import const
 
 
 
@@ -9,6 +12,26 @@ async def start_button(message: types.Message):
     await bot.send_message(
         chat_id=message.from_user.id,
         text=f'Hello {message.from_user.first_name}'
+    )
+
+async def start_button(message: types.Message):
+    print(message)
+    db = bot_db.Database()
+    db.sql_insert_user(
+        tg_id=message.from_user.id,
+        username=message.from_user.username,
+        first_name=message.from_user.first_name,
+        last_name=message.from_user.last_name,
+    )
+
+
+
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=const.START_MENU_TEXT.format(
+            user=message.from_user.first_name
+        ),
+        reply_markup=await start_inline_buttons.start_keyboard()
     )
 
 
---
+++ keyboards/start_inline_buttons.py
@@ -0,0 +1,15 @@
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
+
+async def start_keyboard():
+    markup = InlineKeyboardMarkup()
+    questionnaire_button = InlineKeyboardButton(
+        "Questionnaire",
+        callback_data="start_questionnaire"
+
+    )
+    markup.add(questionnaire_button)
+    return markup
